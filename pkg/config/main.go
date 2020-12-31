d64ee70e46f4f465386407fc803531d79fb23a1f
Rueian <rueiancsie@gmail.com>
2021-01-01 02:26:00 +0800

    chore: change service names

--- pkg/config/main.go
+++ pkg/config/main.go
@@ -17,9 +17,10 @@ type Proxy struct {
 	ProxyPort    uint32 `envconfig:"PROXY_PORT"     default:"18001"`
 	ProxyPortMin uint32 `envconfig:"PROXY_PORT_MIN" default:"20000"`
 	ProxyPortMax uint32 `envconfig:"PROXY_PORT_MAX" default:"32767"`
-	XDSAddr      string `envconfig:"XDS_ADDR"       default:"xds:18000"`
+	XDSAddr      string `envconfig:"XDS_ADDR"       default:"zenvoy-xds:18000"`
 	XDSNodeID    string `envconfig:"XDS_NODE_ID"    default:"zenvoy"`
-	TriggerURL   string `envconfig:"TRIGGER_URL"    default:"http://xds:17999"`
+	TriggerURL   string `envconfig:"TRIGGER_URL"    default:"http://zenvoy-xds:17999"`
+	TPROXYCMD    string `envconfig:"TPROXY_CMD"     default:"iptables"`
 }
 
 func GetXDS() (out XDS, err error) {
